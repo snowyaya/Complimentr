f553d102dbd9bc41c7e0a7e6c7499935ea41f146
Glitch (voracious-navy-moonstone) <none>
2021-10-03 19:22:06 +0000

    🍥🏏 Checkpoint ./app.py:37012741/223

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from flask import (
     request,
     url_for,
 )
+from twilio.rest import Client
 
 load_dotenv()
 app = Flask(__name__)
@@ -16,14 +17,20 @@ app.secret_key = "ssssh don't tell anyone"
 
 TWILIO_PHONE_NUMBER = os.getenv('TWILIO_PHONE_NUMBER')
 
+client Client()
+
 def get_sent_messages():
     # TODO: Make this return a collection of messages that were sent from the number
-    messages = []
+    messages = client.messages.list(from = TWILIO_PHONE_NUMBER)
     return messages
 
 def send_message(to, body):
     # TODO: Send the text message
-    pass
+    client.messages.create(
+      to = to
+      body = body
+      from_ = TWILIO_PHONE_NUMBER
+    )
 
 @app.route("/", methods=["GET"])
 def index():
